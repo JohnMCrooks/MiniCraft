ff8ed6e15c833dd6efc70c654e9208c18484980d
John Crooks <johnmcrooks@gmail.com>
2016-06-02 14:37:30 -0400

    Week 3 Day 4 - HW Assignment Sprite now faces the direction it is moving. when sprite leaves one side of the map he is transported back to the other side.

--- core/src/com/crooks/MyGdxGame.java
+++ core/src/com/crooks/MyGdxGame.java
@@ -13,10 +13,14 @@ public class MyGdxGame extends ApplicationAdapter {
 	float x,y,xv,yv;
 	static final int WIDTH = 16;
 	static final int HEIGHT = 16;
-	static final float decelaration = .80f;
+	static final float decelaration = .50f;  //How quickly the character slides to a stop
+	static final float acceleration = 2f;  //SpaceBar modifier for speed.
 	static final float MAX_VELOCITY = 100;
+	static final float MAX_WIDTH = 800;
+	static final float MAX_HEIGHT = 600;
+	boolean faceRight;
 
-	TextureRegion leftSprite,rightSprite,upSprite,downSprite;
+	TextureRegion rightSprite,upSprite,downSprite;
 
 	@Override
 	public void create () {
@@ -32,26 +36,76 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		move();
+
+		TextureRegion spriteState;
+		if(xv>0){
+			spriteState = rightSprite;
+		}else if (xv<0){
+			spriteState = rightSprite;
+		}else if (yv>0){
+			spriteState = upSprite;
+		} else if (yv<0){
+			spriteState = downSprite;
+		}else{
+			spriteState = downSprite;
+		}
+
 		Gdx.gl.glClearColor(.1f, .8f, .5f, 0.9f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(downSprite,x,y, WIDTH *3, HEIGHT*3);
+
+		if (faceRight){
+			batch.draw(spriteState,x,y, WIDTH *3, HEIGHT*3);
+		}else if(!faceRight){
+			batch.draw(spriteState,(x + WIDTH * 3),y, WIDTH *-3, HEIGHT*3);
+		}else{}
+
+		//Setting Wall Boundries so Character appears on opposite side - A la PacMan
+		if (x >MAX_WIDTH){
+			x = -8;
+			batch.draw(spriteState,x,y, WIDTH *3, HEIGHT*3);
+		}
+		if(x<-16){
+			batch.draw(spriteState,x,y, WIDTH *3, HEIGHT*3);
+		}
+		if (y >MAX_HEIGHT){
+			y = -8;
+			batch.draw(spriteState,x,y, WIDTH *3, HEIGHT*3);
+		}
+		if(y<-16){
+			y = MAX_HEIGHT-8;
+			batch.draw(spriteState,x,y, WIDTH *3, HEIGHT*3);
+		}
 		batch.end();
 	}
 
 	public void move(){
+
 		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
 			yv = MAX_VELOCITY;
+			if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+				yv=accelerate(yv);
+			}
+
 		} else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
 			yv= -MAX_VELOCITY;
+			if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+				yv=accelerate(yv);
+			}
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
 			xv = -MAX_VELOCITY;
-			//faceRight = false;
+			faceRight = false;
+			if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+				xv=accelerate(xv);
+			}
 		}
 		else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
 			xv = MAX_VELOCITY;
-			//faceRight = true;
+			faceRight = true;
+			if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+				xv=accelerate(xv);
+			}
 		}
 		float delta = Gdx.graphics.getDeltaTime();
 		y += yv*delta;
@@ -60,6 +114,11 @@ public class MyGdxGame extends ApplicationAdapter {
 		xv = deaccelarate(xv);
 
 
+	}
+
+	public float accelerate(float velocity){
+		velocity *= acceleration;
+		return velocity;
 	}
 
 	public float deaccelarate(float velocity){
--- desktop/src/com/crooks/desktop/DesktopLauncher.java
+++ desktop/src/com/crooks/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.crooks.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 600;
+		config.width = 800;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
